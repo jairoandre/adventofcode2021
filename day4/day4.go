12bb51c847ee6b416ea2c00122830871dc9d241f
Jairo <jairoandre@gmail.com>
2021-12-07 00:20:58 -0300

    Day 4 completed!

--- day4/day4.go
+++ day4/day4.go
@@ -2,6 +2,7 @@ package day4
 
 import (
 	"bufio"
+	"fmt"
 	"log"
 	"os"
 	"strconv"
@@ -13,10 +14,12 @@ type Cell struct {
 	checked bool
 }
 
-func (c *Cell) Check(val int) {
+func (c *Cell) Check(val int) bool {
 	if c.value == val {
 		c.checked = true
+		return true
 	}
+	return false
 }
 
 func NewCell(val int) *Cell {
@@ -26,28 +29,31 @@ func NewCell(val int) *Cell {
 	return &cell
 }
 
-type Row struct {
-	cells []Cell
+type Cells struct {
+	cells []*Cell
 }
 
-func NewRow() *Row {
-	row := Row{}
-	row.cells = make([]Cell, 0)
+func NewRow() *Cells {
+	row := Cells{}
+	row.cells = make([]*Cell, 0)
 	return &row
 }
 
-func (r *Row) AddCell(val int) {
-	r.cells = append(r.cells, *NewCell(val))
+func (c *Cells) AddCell(cell *Cell) {
+	c.cells = append(c.cells, cell)
 }
 
-func (r *Row) Check(val int) {
-	for _, cell := range r.cells {
-		cell.Check(val)
+func (c *Cells) Check(val int) bool {
+	for idx, _ := range c.cells {
+		if c.cells[idx].Check(val) {
+			return true
+		}
 	}
+	return false
 }
 
-func (r *Row) IsAllChecked() bool {
-	for _, cell := range r.cells {
+func (c *Cells) IsAllChecked() bool {
+	for _, cell := range c.cells {
 		if !cell.checked {
 			return false
 		}
@@ -56,26 +62,60 @@ func (r *Row) IsAllChecked() bool {
 }
 
 type Sheet struct {
-	rows []Row
+	rows []Cells
+	cols []Cells
 }
 
-func (s *Sheet) Check() bool {
+func (s *Sheet) Check(val int) bool {
+	for rowIdx, _ := range s.rows {
+		for cellIdx, _ := range s.rows[rowIdx].cells {
+			if s.rows[rowIdx].cells[cellIdx].Check(val) {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func (s *Sheet) DidBingo() bool {
 	// check rows
 	for _, row := range s.rows {
 		if row.IsAllChecked() {
 			return true
 		}
 	}
+	for _, col := range s.cols {
+		if col.IsAllChecked() {
+			return true
+		}
+	}
 	return false
 }
 
-func (s *Sheet) AddRow(row *Row) {
+func (s *Sheet) Calc(call int) int {
+	sumUnchecked := 0
+	for _, row := range s.rows {
+		for _, cell := range row.cells {
+			if !cell.checked {
+				sumUnchecked += cell.value
+			}
+		}
+	}
+	return sumUnchecked * call
+}
+
+func (s *Sheet) AddRow(row *Cells) {
 	s.rows = append(s.rows, *row)
+}
+
+func (s *Sheet) AddCol(row *Cells) {
+	s.cols = append(s.cols, *row)
 }
 
 func NewSheet() *Sheet {
 	sheet := Sheet{}
-	sheet.rows = make([]Row, 0)
+	sheet.rows = make([]Cells, 0)
+	sheet.cols = make([]Cells, 0)
 	return &sheet
 }
 
@@ -101,14 +141,47 @@ func Solution1() {
 		scanner.Text() // blank line
 		sheet := NewSheet()
 		for i := 0; i < 5; i++ {
+			sheet.AddCol(NewRow())
+		}
+
+
+		for i := 0; i < 5; i++ {
 			row := NewRow()
-			for _, strCell := range strings.Split(scanner.Text(), ",") {
+			scanner.Scan()
+			raw := scanner.Text()
+			strArr := strings.Fields(raw)
+			for idx, strCell := range strArr {
 				intCell, _ := strconv.Atoi(strCell)
-				row.AddCell(intCell)
+				cell := NewCell(intCell)
+				sheet.cols[idx].AddCell(cell)
+				row.AddCell(cell)
 			}
 			sheet.AddRow(row)
 		}
 		sheets = append(sheets, *sheet)
 	}
+
+	countBingos := 0
+
+	winners := make(map[int]bool, 0)
+
+	Exit:
+	for callIdx, call := range calls {
+		for idx, sheet := range sheets {
+			if sheet.Check(call) {
+				if sheet.DidBingo() && !winners[idx] {
+					fmt.Println(fmt.Sprintf("Bingo on sheet %d, call %d at %d. Position %d:", idx, call, callIdx, len(winners)))
+					winners[idx] = true
+					if countBingos == len(sheets) - 1 {
+						fmt.Println(sheet.Calc(call))
+						break Exit
+					}
+					countBingos += 1
+				}
+			}
+		}
+	}
+
+	fmt.Println("Done!")
 
 }
